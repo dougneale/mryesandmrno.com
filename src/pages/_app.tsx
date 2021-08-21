1ce03294047aa20ae07f2b613d0e009857813dd7
shippit <shippit@Krishnas-MacBook-Pro.local>
2021-08-21 21:35:47 +1000

    added tailwind and homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,41 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import 'tailwindcss/tailwind.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <nav className="w-100 p-3 mb-4 space-x-2">
+        <a href="/">
+          <span className="m-0 hover:bg-gray-100 p-2 text-md text-black rounded inline-block">
+            Home
+          </span>
+        </a>
+        <a href="/our-story">
+          <span className="m-0 hover:bg-gray-100 p-2 text-md text-black rounded inline-block">
+            Our Story
+          </span>
+        </a>
+        <a href="/our-food">
+          <span className="m-0 hover:bg-gray-100 p-2 text-md text-black rounded inline-block">
+            Our Food
+          </span>
+        </a>
+        <a href="/recipes">
+          <span className="m-0 hover:bg-gray-100 p-2 text-md text-black rounded inline-block">
+            Recipes
+          </span>
+        </a>
+        <a href="/contact">
+          <span className="m-0 hover:bg-gray-100 p-2 text-md text-black rounded inline-block">
+            Contact Us
+          </span>
+        </a>
+      </nav>
+      <div className="mx-1/2">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,35 @@
-import Layout from "../components/Layout";
-import BasicMeta from "../components/meta/BasicMeta";
-import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
 
 export default function Index() {
   return (
-    <Layout>
-      <BasicMeta url={"/"} />
-      <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
-      <div className="container">
-        <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
-        </div>
+    <div
+    className="container lg:w-2/3 space-y-12 flex-col flex items-center pt-10"
+  >
+    <img className="w-1/3 max-w-md" src="images/Logo.png" />
+    <div>
+      <div className="flex flex-row space-x-9 font-medium text-xl">
+        <a href="/our-story">
+          <span className=" hover:text-blue-800">Our Story</span>
+        </a>
+        <a href="/our-food">
+          <span className=" hover:text-blue-800">Our Food</span>
+        </a>
+        <a href="/contact">
+          <span className=" hover:text-blue-800">Contact Us</span>
+        </a>
       </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
+    </div>
+    <div className="space-x-5">
+      <SocialIcon url="https://www.facebook.com/MrYesandMrNoFacebook" />
+      <SocialIcon url="https://www.instagram.com/mryesandmrno" />
+      <SocialIcon url="mailto:mryesandmrno@gmail.com" />
+    </div>
 
-        @media (min-width: 769px) {
-          h1 {
-            font-size: 3rem;
-          }
-          h2 {
-            font-size: 2.25rem;
-          }
-        }
-      `}</style>
-    </Layout>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/aTNCly7Ylo4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/52jaLNehOu4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
   );
 }
